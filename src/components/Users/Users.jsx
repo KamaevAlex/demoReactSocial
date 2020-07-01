8bb47d1a2034332f5a8d785425da46c3bb5e9988
AlexKamaev <grmizza@gmail.com>
2020-07-01 19:30:07 +0300

    53 class component in Users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,56 +3,48 @@ import s from './users.module.css';
 import axios from 'axios';
 import userPhoto from '../../assets/images/userDefault.png';
 
-let Users = (props) => {
-	let getUsers = () => {
+class Users extends React.Component {
+	// constructor (props) {
+	// 	super(props);
+	// }
 
-		if (props.users.length === 0) { 							// Иначе будет зацикливание 49ур 57мин
-			axios.get("https://social-network.samuraijs.com/api/1.0/users")
-				.then(response => {
-					props.setUsers(response.data.items);
-				});
-		}
+	componentDidMount() {
+		axios.get("https://social-network.samuraijs.com/api/1.0/users")
+			.then(response => {
+				this.props.setUsers(response.data.items);
+			});
 	}
 
-	return <div>
-		<button onClick = {getUsers}>Get Users</button>
+	render() {
+		return <div>
+		
 		{
-			props.users.map( u => <div key={u.id}>
+			this.props.users.map( u => <div key={u.id}>
 				<span>
 					<div>
 						<img src={u.photos.small != null ? u.photos.small : userPhoto} alt="user photo" className = {s.userPhoto}/>
 					</div>
 					<div>
 						{u.followed
-							? <button onClick = {()=> {props.unfollow(u.id)}}>Unfollow</button> 	
-							: <button onClick = {() => {props.follow(u.id)}}>Follow</button>}
-					</div>
-				</span>
-				<span>
-					<span>
-						<div>{u.name}</div>
-						<div>{u.status}</div>
+							? <button onClick = {()=> {this.props.unfollow(u.id)}}>Unfollow</button> 	
+							: <button onClick = {() => {this.props.follow(u.id)}}>Follow</button>}
+						</div>
 					</span>
 					<span>
-						<div>{"u.location.coutry"}</div>
-						<div>{"u.location.city"}</div>
+						<span>
+							<div>{u.name}</div>
+							<div>{u.status}</div>
+						</span>
+						<span>
+							<div>{"u.location.coutry"}</div>
+							<div>{"u.location.city"}</div>
+						</span>
 					</span>
-				</span>
-			</div> )
+					</div> )
 			}
-	</div>
-}
+			</div>
+		}
+	}
+
 
 export default Users;
-
-
-
-
-// props.setUsers ([
-// 			{id:1, photoUrl: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_1677602.jpg', 
-// 			followed: false, fullName: 'Dmitry', status: 'I am a boss', location : {city: 'Minsk', country: 'Belarus'} },
-// 			{id:2, photoUrl: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_1677602.jpg', 
-// 			followed: true, fullName: 'Sasha', status: 'I am a boss too', location : {city: 'Moscow', country: 'Russia'} },
-// 			{id:3, photoUrl: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_1677602.jpg', 
-// 			followed: true, fullName: 'Andrew', status: 'I am a boss too', location : {city: 'Ukraine', country: 'Ukraine'} }]
-// 			);
