2bb33193257e009eb23cb99217a1a7e2f0025847
AlexKamaev <grmizza@gmail.com>
2020-09-01 14:46:37 +0300

    61 Login, cookie

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import s from './header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-	return(
-		<header className = {s.header}>
-			<div className={s.logo}>
-      	<img src={require ("./../../img/logo.png")} alt="logotype"/>
-      </div> 
-     </header>
-		);
+const Header = (props) => {
+    return (
+        <header className={s.header}>
+            <img className={s.logo} src={require("./../../img/logo.png")} alt="logotype"/>
+            <div className={s.loginBlock}>
+
+                {props.isAuth ? props.login
+                    :<NavLink to={'/login'}>Login</NavLink>}
+            </div>
+
+        </header>
+    );
 }
 
 export default Header;
